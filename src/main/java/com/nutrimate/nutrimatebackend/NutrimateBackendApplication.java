c20b9a220786ec868f9381112e4ae6f987b75468
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2024-01-04 14:13:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/nutrimate/nutrimatebackend/NutrimateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrimate.nutrimatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimateBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(NutrimateBackendApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/nutrimate/nutrimatebackend/NutrimateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrimate.nutrimatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimateBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
