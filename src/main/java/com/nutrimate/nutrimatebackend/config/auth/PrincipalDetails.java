e09352dbfdd14330a3bf7a1dbc98fba002755128
uujean12 <uujean12@gmail.com>
2024-01-22 23:22:04 +0900

    Revert "Merge branch 'ICT2-42-login' of https://github.com/ICT-team-2/nutrimate-backend into ICT2-42-login"
    This reverts commit 973339353533b67317456b96a8c945f83e946f49, reversing
    changes made to 1b2c1ccd1ea5985c3f15febd803641798cadb373.

--- src/main/java/com/nutrimate/nutrimatebackend/config/auth/PrincipalDetails.java
+++ src/main/java/com/nutrimate/nutrimatebackend/config/auth/PrincipalDetails.java
@@ -1,13 +1,13 @@
 package com.nutrimate.nutrimatebackend.config.auth;
 
-import com.nutrimate.nutrimatebackend.model.user.MemberDto;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
-import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import com.nutrimate.nutrimatebackend.model.user.MemberDto;
+import lombok.Data;
 
 // 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다
 // 로그인 진행이 완료가 되면 시큐리티 session을 만들어준다.(Security ContextHolder)
@@ -21,73 +21,71 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 public class PrincipalDetails implements UserDetails, OAuth2User {
 
 
-	private MemberDto memberDto;
-	private Map<String, Object> attributes;
+  private MemberDto memberDto;
+  private Map<String, Object> attributes;
 
-	// 일반 로그인
-	public PrincipalDetails(MemberDto memberDto) {
-		this.memberDto = memberDto;
-	}
+  // 일반 로그인
+  public PrincipalDetails(MemberDto memberDto) {
+    this.memberDto = memberDto;
+  }
 
-	// OAuth 로그인
-	public PrincipalDetails(MemberDto memberDto, Map<String, Object> attributes) {
-		this.memberDto = memberDto;
-		this.attributes = attributes;
-	}
+  // OAuth 로그인
+  public PrincipalDetails(MemberDto memberDto, Map<String, Object> attributes) {
+    this.memberDto = memberDto;
+    this.attributes = attributes;
+  }
 
-	@Override
-	public String getName() {
-		return memberDto.getUserId() + "";
-	}
+  @Override
+  public String getName() {
+    return memberDto.getUserName();
+  }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		memberDto.getRoleList().forEach(r -> {
-			authorities.add(() -> {
-				return r;
-			});
-		});
-		return authorities;
-	}
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+    memberDto.getRoleList().forEach(r -> {
+      authorities.add(() -> {
+        return r;
+      });
+    });
+    return authorities;
+  }
 
-	@Override
-	public String getPassword() {
-		return memberDto.getUserPwd() != null
-		       ? memberDto.getUserPwd() : "겟인데어";
-	}
+  @Override
+  public String getPassword() {
+    return memberDto.getUserPwd();
+  }
 
+  @Override
+  public String getUsername() {
+    return memberDto.getUserUid();
+  }
 
-	@Override
-	public String getUsername() {
-		return memberDto.getUserUid();
-	}
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+  @Override
+  public boolean isEnabled() {
+    // 우리사이트 1년동안 회원이 로그인 안하면 휴먼계정
+    // 현재시간 - 로그인시간 => 1년 초과 return false;
+    return true;
+  }
 
-	@Override
-	public boolean isEnabled() {
-		// 우리사이트 1년동안 회원이 로그인 안하면 휴먼계정
-		// 현재시간 - 로그인시간 => 1년 초과 return false;
-		return true;
-	}
-
-	@Override
-	public Map<String, Object> getAttributes() {
-		return attributes;
-	}
+  @Override
+  public Map<String, Object> getAttributes() {
+    return attributes;
+  }
 
 }
--- src/main/java/com/nutrimate/nutrimatebackend/mapper/MemberMapper.java
+++ src/main/java/com/nutrimate/nutrimatebackend/mapper/MemberMapper.java
@@ -1,21 +1,22 @@
 package com.nutrimate.nutrimatebackend.mapper;
 
-import com.nutrimate.nutrimatebackend.model.user.MemberDto;
 import org.apache.ibatis.annotations.Mapper;
+import com.nutrimate.nutrimatebackend.model.user.MemberDto;
 
 @Mapper
 public interface MemberMapper {
 
-	int insertCommonMember(MemberDto memberDto);
+  int insertCommonMember(MemberDto memberDto);
 
-	int insertMember(MemberDto memberDto);
+  int insertMember(MemberDto memberDto);
 
-	int insertMemberIdAndEmail(MemberDto memberDto);
+  int insertMemberId(MemberDto memberDto);
 
-	int insertOAuthMember(MemberDto memberDto);
+  int insertOAuthMember(MemberDto memberDto);
 
-	MemberDto findCommonMemberByUid(String userUid);
+  MemberDto findCommonMemberByUid(String userUid);
 
-	MemberDto findOAuthMemberByProviderId(String providerId);
+  MemberDto findOAuthMemberByProviderId(String providerId);
+
 
 }
