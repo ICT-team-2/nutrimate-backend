09cb87995333aac09e45766c770e4c88392244db
uujean12 <uujean12@gmail.com>
2024-01-22 23:23:56 +0900

    Revert "Revert "Merge branch 'ICT2-42-login' of https://github.com/ICT-team-2/nutrimate-backend into ICT2-42-login""
    This reverts commit e09352dbfdd14330a3bf7a1dbc98fba002755128.

--- src/main/java/com/nutrimate/nutrimatebackend/service/MemberService.java
+++ src/main/java/com/nutrimate/nutrimatebackend/service/MemberService.java
@@ -1,41 +1,41 @@
 package com.nutrimate.nutrimatebackend.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import com.nutrimate.nutrimatebackend.mapper.MemberMapper;
 import com.nutrimate.nutrimatebackend.model.user.MemberDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
 @Log4j2
 public class MemberService {
-  private MemberMapper memberMapper;
-  private PasswordEncoder passwordEncoder;
+	private MemberMapper memberMapper;
+	private PasswordEncoder passwordEncoder;
 
-  public MemberService(MemberMapper memberMapper, PasswordEncoder passwordEncoder) {
-    this.memberMapper = memberMapper;
-    this.passwordEncoder = passwordEncoder;
-  }
+	public MemberService(MemberMapper memberMapper, PasswordEncoder passwordEncoder) {
+		this.memberMapper = memberMapper;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-  @Transactional
-  public int insertMember(MemberDto memberDto) {
-    memberMapper.insertMember(memberDto);
-    log.info("memberDto: " + memberDto);
-    return memberMapper.insertCommonMember(memberDto);
-  }
+	@Transactional
+	public int insertMember(MemberDto memberDto) {
+		memberMapper.insertMember(memberDto);
+		log.info("memberDto: " + memberDto);
+		return memberMapper.insertCommonMember(memberDto);
+	}
 
-  public MemberDto findCommonMemberByUid(String userUid) {
-    MemberDto memberDto = memberMapper.findCommonMemberByUid(userUid);
-    log.info("memberDto: " + memberDto);
-    return memberDto;
-  }
+	public MemberDto findCommonMemberByUid(String userUid) {
+		MemberDto memberDto = memberMapper.findCommonMemberByUid(userUid);
+		log.info("memberDto: " + memberDto);
+		return memberDto;
+	}
 
-  @Transactional
-  public MemberDto insertOAuthMember(MemberDto memberDto) {
-    memberMapper.insertMemberId(memberDto);
-    memberMapper.insertOAuthMember(memberDto);
-    return memberDto;
-  }
+	@Transactional
+	public MemberDto insertOAuthMember(MemberDto memberDto) {
+		memberMapper.insertMemberIdAndEmail(memberDto);
+		memberMapper.insertOAuthMember(memberDto);
+		return memberDto;
+	}
 }
