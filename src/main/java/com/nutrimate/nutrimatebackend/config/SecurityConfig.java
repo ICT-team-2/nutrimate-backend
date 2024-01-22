7cda08a45e4c7d5bfce175bbb94e346817b9e79c
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2024-01-22 23:19:26 +0900

    principalName cannot be empty 에러는 해결, 401에러 발생

--- src/main/java/com/nutrimate/nutrimatebackend/config/SecurityConfig.java
+++ src/main/java/com/nutrimate/nutrimatebackend/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.nutrimate.nutrimatebackend.config;
 
+import com.nutrimate.nutrimatebackend.config.jwt.JwtAuthenticationFilter;
+import com.nutrimate.nutrimatebackend.config.jwt.JwtAuthorizationFilter;
+import com.nutrimate.nutrimatebackend.config.oauth.LoggingOAuth2LoginAuthenticationFilter;
+import com.nutrimate.nutrimatebackend.config.oauth.PrincipalOauth2UserService;
+import com.nutrimate.nutrimatebackend.mapper.MemberMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,64 +16,83 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.filter.CorsFilter;
-import com.nutrimate.nutrimatebackend.config.jwt.JwtAuthenticationFilter;
-import com.nutrimate.nutrimatebackend.config.jwt.JwtAuthorizationFilter;
-import com.nutrimate.nutrimatebackend.config.oauth.PrincipalOauth2UserService;
-import com.nutrimate.nutrimatebackend.mapper.MemberMapper;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Log4j2
 public class SecurityConfig {
 
-  private PrincipalOauth2UserService principalOauth2UserService;
-  private CorsFilter corsFilter;
-  private MemberMapper memberMapper;
+	@Autowired
+	ClientRegistrationRepository clientRegistrationRepository;
+	@Autowired
+	OAuth2AuthorizedClientService authorizedClientService;
 
-  public SecurityConfig(CorsFilter corsFilter,
-      PrincipalOauth2UserService principalOauth2UserService, MemberMapper memberMapper) {
-    this.corsFilter = corsFilter;
-    this.principalOauth2UserService = principalOauth2UserService;
-    this.memberMapper = memberMapper;
-  }
+	private PrincipalOauth2UserService principalOauth2UserService;
+	private CorsFilter corsFilter;
+	private MemberMapper memberMapper;
+	public SecurityConfig(CorsFilter corsFilter,
+	                      PrincipalOauth2UserService principalOauth2UserService,
+	                      MemberMapper memberMapper) {
+		this.corsFilter = corsFilter;
+		this.principalOauth2UserService = principalOauth2UserService;
+		this.memberMapper = memberMapper;
+	}
+	@Bean
+	static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http,
+	                                        AuthenticationManager authenticationManager)
+			throws Exception {
 
+		LoggingOAuth2LoginAuthenticationFilter loggingFilter =
+				new LoggingOAuth2LoginAuthenticationFilter(clientRegistrationRepository,
+				                                           authorizedClientService);
+		loggingFilter.setAuthenticationManager(authenticationManager);
 
-  @Bean
-  AuthenticationManager authenticationManager(
-      AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
-  }
+		http.addFilterBefore(corsFilter, ChannelProcessingFilter.class);
+		http.csrf(t -> t.disable())
+		    .sessionManagement((sessionManagement) -> sessionManagement
+				    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		    .addFilter(corsFilter) // @CrossOrigin(인증 x), 시큐리티 필터에 등록인증(o)
+		    .addFilter(new JwtAuthenticationFilter(authenticationManager)) // AuthenticationManager
+		    .addFilter(new JwtAuthorizationFilter(authenticationManager,
+		                                          memberMapper)) // AuthenticationManager
+		    .authorizeHttpRequests(t -> t.requestMatchers("/api/v1/user/**").authenticated()
+		                                 .requestMatchers("/api/v1/user/**")
+		                                 .hasAnyRole("USER", "MANAGER", "ADMIN")
+		                                 .requestMatchers("/api/v1/manager/**")
+		                                 .hasAnyRole("MANAGER", "ADMIN")
+		                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+		                                 .anyRequest().permitAll())
+		    .formLogin(
+				    t -> t.loginPage("/loginForm").loginProcessingUrl("/login")
+				          .defaultSuccessUrl("/"))
+		    .oauth2Login(t -> t.loginPage("/loginForm")
+		                       .userInfoEndpoint(endpoint -> endpoint.userService(
+				                       principalOauth2UserService))
+		                       .failureHandler((request, response, exception) -> {
+			                       log.info("exception: ", exception);
+			                       response.getWriter().println(exception.getMessage());
+		                       }))
+		    .addFilterAfter(loggingFilter, LogoutFilter.class);
+		;
 
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http,
-      AuthenticationManager authenticationManager) throws Exception {
-    http.addFilterBefore(corsFilter, ChannelProcessingFilter.class);
-    http.csrf(t -> t.disable())
-        .sessionManagement((sessionManagement) -> sessionManagement
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .addFilter(corsFilter) // @CrossOrigin(인증 x), 시큐리티 필터에 등록인증(o)
-        .addFilter(new JwtAuthenticationFilter(authenticationManager)) // AuthenticationManager
-        .addFilter(new JwtAuthorizationFilter(authenticationManager, memberMapper)) // AuthenticationManager
-        .authorizeHttpRequests(t -> t.requestMatchers("/api/v1/user/**").authenticated()
-            .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN")
-            .requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN")
-            .requestMatchers("/api/v1/admin/**").hasRole("ADMIN").anyRequest().permitAll())
-        .formLogin(
-            t -> t.loginPage("/loginForm").loginProcessingUrl("/login").defaultSuccessUrl("/"))
-        .oauth2Login(t -> t.loginPage("/loginForm")
-            .userInfoEndpoint(endpoint -> endpoint.userService(principalOauth2UserService)));
-
-    return http.build();
-  }
-
-
-  @Bean
-  static BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+		return http.build();
+	}
 
 }
