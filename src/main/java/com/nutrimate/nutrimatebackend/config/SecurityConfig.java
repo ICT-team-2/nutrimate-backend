1bae3514fdb51daaac85bbe4868dfdaaec5b93ec
jaehak26 <jack10829@naver.com>
2024-01-30 21:16:38 +0900

    ICT2-51 폴더구조 변경

--- src/main/java/com/nutrimate/nutrimatebackend/config/SecurityConfig.java
+++ src/main/java/com/nutrimate/nutrimatebackend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.nutrimate.nutrimatebackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 //todo: 아래 내용 수정하여 사용할 것!!
@@ -12,12 +13,12 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable();
+		http.csrf(AbstractHttpConfigurer::disable);
 		http.authorizeRequests()
 				//url에 접근 권한 설정
-				.antMatchers("/user/**").authenticated()
-				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+//				.antMatchers("/user/**").authenticated()
+//				.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+//				.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 				.anyRequest().permitAll()
 		;
 		
--- src/main/java/com/nutrimate/nutrimatebackend/model/board/diet/FileUtils.java
+++ src/main/java/com/nutrimate/nutrimatebackend/model/board/diet/FileUtils.java
@@ -1,10 +1,10 @@
-package com.nutrimate.nutrimatebackend.model.infoboard;
+package com.nutrimate.nutrimatebackend.model.board.diet;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
-
 
 
 public class FileUtils {
@@ -17,7 +17,7 @@ public class FileUtils {
 		} else {
 			String ext = fileName.substring(fileName.lastIndexOf(".") + 1).trim();
 			String fileNameExcludeExt = fileName.substring(0, fileName.lastIndexOf("."));
-
+			
 			String newFileName;
 			while (true) {
 				newFileName = "";
@@ -46,47 +46,46 @@ public class FileUtils {
 				} else
 					break;
 			} //////////// while
-
+			
 			return newFileName;
 		}
 	}/////////////////////
-
+	
 	//업로드 로직
-	public static StringBuffer upload(List<MultipartFile> files,String saveDirectory) throws IllegalStateException, IOException {
+	public static StringBuffer upload(List<MultipartFile> files, String saveDirectory) throws IllegalStateException, IOException {
 		//업로드된 파일명들 저장용
-		StringBuffer fileNames=new StringBuffer();
-        
-		try {           
-		    
-		  
-		  for(MultipartFile file:files) {
-          if(file.getContentType() !=null) {//input type="file"인 경우               
-            String newFilename=FileUtils.getNewFileName(saveDirectory, file.getOriginalFilename());
-                   File file_ = new File(saveDirectory+File.separator+newFilename);
-                   //해당 폴더 없으면 자동 생성
-                   if(!file_.exists()){
-                     file_.mkdirs();
-                   }
-                   file.transferTo(file_);
-              //파일명을 스프링버퍼에 저장
-                   fileNames.append("/imageLoading/"+newFilename+",");//구분자 , 
-                  }
-            }
+		StringBuffer fileNames = new StringBuffer();
+		
+		try {
+			
+			
+			for (MultipartFile file : files) {
+				if (file.getContentType() != null) {//input type="file"인 경우
+					String newFilename = FileUtils.getNewFileName(saveDirectory, file.getOriginalFilename());
+					File file_ = new File(saveDirectory + File.separator + newFilename);
+					//해당 폴더 없으면 자동 생성
+					if (!file_.exists()) {
+						file_.mkdirs();
+					}
+					file.transferTo(file_);
+					//파일명을 스프링버퍼에 저장
+					fileNames.append("/imageLoading/" + newFilename + ",");//구분자 ,
+				}
+			}
+		} catch (Exception e) {//파일 미 첨부시 에러시:null반환
+			e.printStackTrace();
+			return null;
 		}
-            catch(Exception e) {//파일 미 첨부시 에러시:null반환   
-                e.printStackTrace();     
-                return null;
-            }
-		return fileNames.deleteCharAt(fileNames.length()-1);
+		return fileNames.deleteCharAt(fileNames.length() - 1);
 	}
-
+	
 	//삭제 로직
-	public static void deletes(StringBuffer fileNames,String saveDirectory,String delim) {
-		String[] files=fileNames.toString().split(delim);
-		for(String filename:files) {
-		    filename=filename.replaceFirst("/imageLoading/","");
-			File f = new File(saveDirectory+File.separator+filename);
-			if(f.exists()) f.delete();
+	public static void deletes(StringBuffer fileNames, String saveDirectory, String delim) {
+		String[] files = fileNames.toString().split(delim);
+		for (String filename : files) {
+			filename = filename.replaceFirst("/imageLoading/", "");
+			File f = new File(saveDirectory + File.separator + filename);
+			if (f.exists()) f.delete();
 		}
 	}
 	
