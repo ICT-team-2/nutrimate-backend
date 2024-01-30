5203b1b9b766553c601ef0e9aa6c9568a95aea0e
jaehak26 <jack10829@naver.com>
2024-01-30 15:00:53 +0900

    ICT2-54 신고 코드 리뷰 및 수정

--- src/main/java/com/nutrimate/nutrimatebackend/mapper/report/ReportMapper.java
+++ src/main/java/com/nutrimate/nutrimatebackend/mapper/report/ReportMapper.java
@@ -1,15 +1,20 @@
 package com.nutrimate.nutrimatebackend.mapper.report;
 
+import com.nutrimate.nutrimatebackend.model.report.ReportDto;
 import org.apache.ibatis.annotations.Mapper;
-import com.nutrimate.nutrimatebackend.mode.report.ReportDto;
+
 @Mapper
 public interface ReportMapper {
-
+	
 	int findByUserIdAndBoarderId(ReportDto dto);//중복신고확인(게시글)
+	
 	int findByUserIdAndCmtId(ReportDto dto);//중복신고확인(댓글)
+	
 	int insertReport(ReportDto dto);//신고테이블에 insert
+	
 	int insertCommentReport(ReportDto dto);//댓글신고
+	
 	int insertBorderReport(ReportDto dto);//글신고
 	
-
+	
 }
