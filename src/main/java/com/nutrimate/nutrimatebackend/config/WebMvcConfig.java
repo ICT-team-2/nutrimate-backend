5cc5a64832302f28c69848750c9a6489e85cee10
jaehak26 <jack10829@naver.com>
2024-02-15 13:49:07 +0900

    ICT2-62 로그아웃 문제 해결(cors)

--- src/main/java/com/nutrimate/nutrimatebackend/config/WebMvcConfig.java
+++ src/main/java/com/nutrimate/nutrimatebackend/config/WebMvcConfig.java
@@ -7,16 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/",
-        "classpath:/templates/");
-  }
-
-  // cors 설정
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("http://localhost:5555").allowedMethods("*")
-        .allowedHeaders("*").allowCredentials(true);
-  }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/",
+				"classpath:/templates/");
+	}
+	
+	// cors 설정
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowCredentials(true)
+				.allowedOrigins("http://localhost:5555")
+				.allowedMethods("*")
+				.allowedHeaders("*");
+	}
 }
