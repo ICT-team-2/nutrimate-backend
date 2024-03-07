aa474e3a73bcb304a925c13edb677293f4455a72
jaehak26 <jack10829@naver.com>
2024-03-07 22:41:31 +0900

    ICT2-90 부적합한 식별자 에러 해결

--- src/main/java/com/nutrimate/nutrimatebackend/model/record/FoodRecordDto.java
+++ src/main/java/com/nutrimate/nutrimatebackend/model/record/FoodRecordDto.java
@@ -17,6 +17,6 @@ public class FoodRecordDto extends FoodDto {
 	RecordDto record;
 	private Integer dietId;// 식단 기록 번호
 	private MealTime mealTime; // 식사 시간
-	private Integer recordIntake; // 섭취량
+	private Integer recordIntake = 200; // 섭취량
 	private double serving; //몇인분인지?
 }
