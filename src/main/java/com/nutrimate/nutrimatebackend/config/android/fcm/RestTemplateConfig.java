27969f9938c36268f28fcfc2330b86100aedcf10
Songjihyeong <jihyeong47@gmail.com>
2024-03-12 13:53:26 +0900

    ICT2-101 FCM 완료

--- src/main/java/com/nutrimate/nutrimatebackend/config/android/fcm/RestTemplateConfig.java
+++ src/main/java/com/nutrimate/nutrimatebackend/config/android/fcm/RestTemplateConfig.java
@@ -16,13 +16,6 @@ public class RestTemplateConfig {
 
         // 2. HttpClient 커스터마이징
         restTemplateBuilder.setConnectTimeout(Duration.ofMillis(3000)); // 요청 타임아웃 시간 설정
-        // restTemplateBuilder.setHttpClient(
-        // HttpClientBuilder.create()
-        // .setMaxConnTotal(50) // 연결을 유지할 최대 Http커넥션 수
-        // .setMaxConnPerRoute(50) // Route당(요청 URI주소당) 최대 Http커넥션 수
-        // .setConnectionTimeToLive(5, TimeUnit.SECONDS) // 커넥션 연결 유지시간
-        // .build()
-        // );
 
         // 3. RestTemplate 객체 생성 및 반환
         return restTemplateBuilder.build();
