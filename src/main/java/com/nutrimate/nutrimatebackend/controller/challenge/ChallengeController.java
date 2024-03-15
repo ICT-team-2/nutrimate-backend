6066046a4610f873bc6b02d49cc9950338380af1
Songjihyeong <jihyeong47@gmail.com>
2024-03-15 18:36:21 +0900

    ICT2-안드로이드 브랜치-챌린지 수정

--- src/main/java/com/nutrimate/nutrimatebackend/controller/challenge/ChallengeController.java
+++ src/main/java/com/nutrimate/nutrimatebackend/controller/challenge/ChallengeController.java
@@ -1,22 +1,27 @@
 package com.nutrimate.nutrimatebackend.controller.challenge;
 
-import com.nutrimate.nutrimatebackend.model.challenge.ChallengeChatDto;
-import com.nutrimate.nutrimatebackend.model.challenge.ChallengeCommentDto;
-import com.nutrimate.nutrimatebackend.service.challenge.ChallengeChatService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.Console;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.nutrimate.nutrimatebackend.model.challenge.ChallengeChatDto;
+import com.nutrimate.nutrimatebackend.model.challenge.ChallengeCommentDto;
+import com.nutrimate.nutrimatebackend.service.challenge.ChallengeChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RestController
@@ -24,182 +29,183 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
 public class ChallengeController {
-	
-	@Autowired
-	private final ChallengeChatService challengeService;
-	@Autowired
-	private final SimpMessageSendingOperations simpMessageSendingOperations;
-	
-	
-	//채팅 참여여부 확인
-	@PostMapping("/chat/member")
-	public ResponseEntity<Map<String, Object>> findAccountChallenge(ChallengeChatDto dto) {
-		Map<String, Object> challengeChatMemeber = new HashMap<>();
-		//채팅에 참여해 본적이 있는지 확인
-		int count = challengeService.countChatMember(dto);
-		if (count == 1) {
-			dto = challengeService.selectChatMember(dto);
-			challengeChatMemeber.put("userId", dto.getUserId());
-			challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
-			challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
-			challengeChatMemeber.put("memberOk", 1);
-			return ResponseEntity.ok(challengeChatMemeber);
-		} else {
-			count = challengeService.selectChallenge(dto);//다른 채팅이라도 챌린지 참여한적 있는 지 확인
-			if (count == 1) {
-				int affect = challengeService.createAccountChat(dto);
-				if (affect == 1) {
-					
-					dto = challengeService.selectChatMember(dto);
-					challengeChatMemeber.put("userId", dto.getUserId());
-					challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
-					challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
-					challengeChatMemeber.put("memberOk", 1);
-					return ResponseEntity.ok(challengeChatMemeber);
-				} else {
-					challengeChatMemeber.put("memberOk", -1);//채팅 참여 실패 알람 띄움
-					return ResponseEntity.ok(challengeChatMemeber);
-					
-				}
-			}
-			challengeChatMemeber.put("memberOk", 0);//챌린지 참여한적 아예 x 닉네임 입력 란으로 이동
-			return ResponseEntity.ok(challengeChatMemeber);
-		}
-		
-		
-	}
-	
-	//챌린지 계정 만들기--계정있는 지 확인한
-	@PostMapping("/account")
-	public ResponseEntity<Map<String, Object>> createAccountChallenge(@RequestBody ChallengeChatDto dto) {
-		Map<String, Object> challengeChatMemeber = new HashMap<>();
-		//챌린지 중복 확인
-		int count = challengeService.selectchallengeAccount(dto);
-		if (count == 0) {
-			int affect = challengeService.createAccountChallenge(dto);
-			if (affect == 1) {
-				dto = challengeService.selectChatMember(dto);
-				challengeChatMemeber.put("userId", dto.getUserId());
-				challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
-				challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
-				challengeChatMemeber.put("memberOk", 1);
-				return ResponseEntity.ok(challengeChatMemeber);
-			} else {
-				challengeChatMemeber.put("memberNotOk", "계정을 만드는 것을 실패했습니다.");
-				return ResponseEntity.ok(challengeChatMemeber);
-			}
-			
-		}
-		challengeChatMemeber.put("memberDupl", "중복된 닉네임이 있습니다.");
-		return ResponseEntity.ok(challengeChatMemeber);
-		
-	}
-	
-	
-	//이전의 모든 메세지를 가져오기
-	@GetMapping("/chat/prev")
-	public List<ChallengeChatDto> getAllPrevMessage(ChallengeChatDto dto) {
-		List<ChallengeChatDto> list = challengeService.getAllPrevMessage(dto);
-		log.info("getAllPrevMessage : " + list);
-		return list;
-	}
-	
-	
-	//메세지 보내주기 및 메세지 저장
-	@MessageMapping("/chat/{roomType}")
-	public void sendMessage(@DestinationVariable String roomType, ChallengeChatDto message) {
-		log.info("sendMessage : " + message);
-		simpMessageSendingOperations.convertAndSend("/sub/channel/" + roomType, message);
-		log.info("sendMessage : " + message);
-		//message.setUserId(challengeService.chatIdSave(message));
-		challengeService.chatRoomSave(message);
-	}
-	
-	
-	//챌린지 성공 기록하기
-	@GetMapping("/success/record")
-	public Map challengeSuccessRecord(@ModelAttribute ChallengeChatDto dto) {
-		Map map = new HashMap();
-		int count = challengeService.countChallengeSuccess(dto);
-		if (count == 0) {
-			int affect = challengeService.saveChallengeSuccess(dto);
-			if (affect == 1) {
-				map.put("SUCCESS", 1);
-			} else {
-				map.put("SUCCESSNOT", 1);
-			}
-		}
-		if (count == 1) {
-			map.put("SUCCESS", 0);//이미 챌린지에 참여했습니다.
-		}
-		return map;
-	}
-	
-	//챌린지 등수
-	@GetMapping("/success")
-	public List<ChallengeChatDto> challengeSuccessList(ChallengeChatDto dto) {
-		return challengeService.getChallengeSuccessList(dto);
-	}
-	
-	
-	//챌린지 댓글 등록
-	@PostMapping("/comment/record")
-	public Map challengeCommentRecord(@RequestBody ChallengeCommentDto dto) {
-		Map map = new HashMap();
-	    //int count = challengeService.countChallengeSuccess(dto);
-		//if (count == 0) {}
-		    System.out.println(dto);
-			int affect = challengeService.savechallengeComment(dto);
-			if (affect == 1) {
-				map.put("SUCCESS", 1);
-				map.put("cmtId",dto.getCmtId());
-			} else {
-				map.put("SUCCESSNOT", 1);
-			}
-		return map;
-	}
-	
-	
-	
-	//챌린지 댓글 가지고 오기
-	@GetMapping("/comment/list")
-	public List<ChallengeCommentDto> challengeCommentList(ChallengeCommentDto dto) {
-		return challengeService.getChallengeCommentList(dto);
-	}
-	
-	//챌린지 댓글 수정
-	@PutMapping("/comment/list/edit")
-	public Map challengeCommentEdit(@RequestBody ChallengeCommentDto dto) {
-	  
-		Map map = new HashMap();
-		int affect = challengeService.editChallengeComment(dto);
-		if (affect == 1) {
-			map.put("SUCCESS", 1);
-		} else {
-			map.put("SUCCESSNOT", 1);
-		}
-		return map;
-	}
-	
-	//챌린지 댓글 삭제
-	@PutMapping("/comment/list/delete")
-	public Map challengeCommentDelete(@RequestBody ChallengeCommentDto dto) {
-		System.out.println(dto.getCmtId());
-		Map map = new HashMap();
-		int affect = challengeService.deleteChallengeComment(dto);
-		if (affect == 1) {
-			map.put("SUCCESS", 1);
-		} else {
-			map.put("SUCCESSNOT", 1);
-		}
-		return map;
-	}
-	
-	//챌린지 유저 nick찾기
-	@GetMapping("/usernick")
-	public Map findUserNick(ChallengeCommentDto dto) {
-		return challengeService.findUserNickByUserId(dto);
-	}
-	
-	
+
+    @Autowired
+    private final ChallengeChatService challengeService;
+    @Autowired
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+
+    // 채팅 참여여부 확인
+    @PostMapping("/chat/member")
+    public ResponseEntity<Map<String, Object>> findAccountChallenge(ChallengeChatDto dto) {
+        Map<String, Object> challengeChatMemeber = new HashMap<>();
+        // 채팅에 참여해 본적이 있는지 확인
+        int count = challengeService.countChatMember(dto);
+        if (count == 1) {
+            dto = challengeService.selectChatMember(dto);
+            challengeChatMemeber.put("userId", dto.getUserId());
+            challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
+            challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
+            challengeChatMemeber.put("memberOk", 1);
+            return ResponseEntity.ok(challengeChatMemeber);
+        } else {
+            count = challengeService.selectChallenge(dto);// 다른 채팅이라도 챌린지 참여한적 있는 지 확인
+            if (count == 1) {
+                int affect = challengeService.createAccountChat(dto);
+                if (affect == 1) {
+
+                    dto = challengeService.selectChatMember(dto);
+                    challengeChatMemeber.put("userId", dto.getUserId());
+                    challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
+                    challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
+                    challengeChatMemeber.put("memberOk", 1);
+                    return ResponseEntity.ok(challengeChatMemeber);
+                } else {
+                    challengeChatMemeber.put("memberOk", -1);// 채팅 참여 실패 알람 띄움
+                    return ResponseEntity.ok(challengeChatMemeber);
+
+                }
+            }
+            challengeChatMemeber.put("memberOk", 0);// 챌린지 참여한적 아예 x 닉네임 입력 란으로 이동
+            return ResponseEntity.ok(challengeChatMemeber);
+        }
+
+
+    }
+
+    // 챌린지 계정 만들기--계정있는 지 확인한
+    @PostMapping("/account")
+    public ResponseEntity<Map<String, Object>> createAccountChallenge(
+            @RequestBody ChallengeChatDto dto) {
+        Map<String, Object> challengeChatMemeber = new HashMap<>();
+        // 챌린지 중복 확인
+        int count = challengeService.selectchallengeAccount(dto);
+        if (count == 0) {
+            int affect = challengeService.createAccountChallenge(dto);
+            if (affect == 1) {
+                dto = challengeService.selectChatMember(dto);
+                challengeChatMemeber.put("userId", dto.getUserId());
+                challengeChatMemeber.put("chatRoomId", dto.getChatroomId());
+                challengeChatMemeber.put("challengeNick", dto.getChallengeNick());
+                challengeChatMemeber.put("memberOk", 1);
+                return ResponseEntity.ok(challengeChatMemeber);
+            } else {
+                challengeChatMemeber.put("memberNotOk", "계정을 만드는 것을 실패했습니다.");
+                return ResponseEntity.ok(challengeChatMemeber);
+            }
+
+        }
+        challengeChatMemeber.put("memberDupl", "중복된 닉네임이 있습니다.");
+        return ResponseEntity.ok(challengeChatMemeber);
+
+    }
+
+
+    // 이전의 모든 메세지를 가져오기
+    @GetMapping("/chat/prev")
+    public List<ChallengeChatDto> getAllPrevMessage(ChallengeChatDto dto) {
+        List<ChallengeChatDto> list = challengeService.getAllPrevMessage(dto);
+        log.info("getAllPrevMessage : " + list);
+        return list;
+    }
+
+
+    // 메세지 보내주기 및 메세지 저장
+    @MessageMapping("/chat/{roomType}")
+    public void sendMessage(@DestinationVariable String roomType, ChallengeChatDto message) {
+        log.info("sendMessage : " + message);
+        simpMessageSendingOperations.convertAndSend("/sub/channel/" + roomType, message);
+        log.info("sendMessage : " + message);
+        // message.setUserId(challengeService.chatIdSave(message));
+        challengeService.chatRoomSave(message);
+    }
+
+
+    // 챌린지 성공 기록하기
+    @GetMapping("/success/record")
+    public Map challengeSuccessRecord(@ModelAttribute ChallengeChatDto dto) {
+        Map map = new HashMap();
+        int count = challengeService.countChallengeSuccess(dto);
+        if (count == 0) {
+            int affect = challengeService.saveChallengeSuccess(dto);
+            if (affect == 1) {
+                map.put("SUCCESS", 1);
+            } else {
+                map.put("SUCCESSNOT", 1);
+            }
+        }
+        if (count == 1) {
+            map.put("SUCCESS", 0);// 이미 챌린지에 참여했습니다.
+        }
+        return map;
+    }
+
+    // 챌린지 등수
+    @GetMapping("/success")
+    public List<ChallengeChatDto> challengeSuccessList(@RequestParam("chatroomId") int chatroomId) {
+        return challengeService.getChallengeSuccessList(chatroomId);
+    }
+
+
+    // 챌린지 댓글 등록
+    @PostMapping("/comment/record")
+    public Map challengeCommentRecord(@RequestBody ChallengeCommentDto dto) {
+        Map map = new HashMap();
+        // int count = challengeService.countChallengeSuccess(dto);
+        // if (count == 0) {}
+        System.out.println(dto);
+        int affect = challengeService.savechallengeComment(dto);
+        if (affect == 1) {
+            map.put("SUCCESS", 1);
+            map.put("cmtId", dto.getCmtId());
+        } else {
+            map.put("SUCCESSNOT", 1);
+        }
+        return map;
+    }
+
+
+
+    // 챌린지 댓글 가지고 오기
+    @GetMapping("/comment/list")
+    public List<ChallengeCommentDto> challengeCommentList(ChallengeCommentDto dto) {
+        return challengeService.getChallengeCommentList(dto);
+    }
+
+    // 챌린지 댓글 수정
+    @PutMapping("/comment/list/edit")
+    public Map challengeCommentEdit(@RequestBody ChallengeCommentDto dto) {
+
+        Map map = new HashMap();
+        int affect = challengeService.editChallengeComment(dto);
+        if (affect == 1) {
+            map.put("SUCCESS", 1);
+        } else {
+            map.put("SUCCESSNOT", 1);
+        }
+        return map;
+    }
+
+    // 챌린지 댓글 삭제
+    @PutMapping("/comment/list/delete")
+    public Map challengeCommentDelete(@RequestBody ChallengeCommentDto dto) {
+        System.out.println(dto.getCmtId());
+        Map map = new HashMap();
+        int affect = challengeService.deleteChallengeComment(dto);
+        if (affect == 1) {
+            map.put("SUCCESS", 1);
+        } else {
+            map.put("SUCCESSNOT", 1);
+        }
+        return map;
+    }
+
+    // 챌린지 유저 nick찾기
+    @GetMapping("/usernick")
+    public Map findUserNick(ChallengeCommentDto dto) {
+        return challengeService.findUserNickByUserId(dto);
+    }
+
+
 }
--- src/main/java/com/nutrimate/nutrimatebackend/service/challenge/ChallengeChatService.java
+++ src/main/java/com/nutrimate/nutrimatebackend/service/challenge/ChallengeChatService.java
@@ -2,103 +2,101 @@ package com.nutrimate.nutrimatebackend.service.challenge;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import com.nutrimate.nutrimatebackend.mapper.challenge.ChallengeChatMapper;
 import com.nutrimate.nutrimatebackend.model.challenge.ChallengeChatDto;
 import com.nutrimate.nutrimatebackend.model.challenge.ChallengeCommentDto;
-
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
 public class ChallengeChatService {
-	
-	private final ChallengeChatMapper challengeChatMapper;
-	
-	public ChallengeChatService(ChallengeChatMapper challengeChatMapper) {
-		this.challengeChatMapper = challengeChatMapper;
-	}
 
-	
-	public int deleteChat(ChallengeChatDto dto) {
-		return challengeChatMapper.deleteChat(dto);
-	}
-	
-    //채팅에 참여해 본적이 있는지 확인
+    private final ChallengeChatMapper challengeChatMapper;
+
+    public ChallengeChatService(ChallengeChatMapper challengeChatMapper) {
+        this.challengeChatMapper = challengeChatMapper;
+    }
+
+
+    public int deleteChat(ChallengeChatDto dto) {
+        return challengeChatMapper.deleteChat(dto);
+    }
+
+    // 채팅에 참여해 본적이 있는지 확인
     public int countChatMember(ChallengeChatDto dto) {
-      return challengeChatMapper.findChatMemberbyUseridAndUserRoom(dto);
+        return challengeChatMapper.findChatMemberbyUseridAndUserRoom(dto);
     }
-    
-    //채팅 참여 정보 가지고 오기
+
+    // 채팅 참여 정보 가지고 오기
     public ChallengeChatDto selectChatMember(ChallengeChatDto dto) {
-      return challengeChatMapper.findChatMemberInfoByUserIdAndUserRoom(dto);
+        return challengeChatMapper.findChatMemberInfoByUserIdAndUserRoom(dto);
     }
-    
-    //챌린지 한 적 있는지 확인
+
+    // 챌린지 한 적 있는지 확인
     public int selectChallenge(ChallengeChatDto dto) {
-      return challengeChatMapper.findChallengeMemberByUserId(dto);
+        return challengeChatMapper.findChallengeMemberByUserId(dto);
     }
-    
-    //채팅방참여 생성
+
+    // 채팅방참여 생성
     public int createAccountChat(ChallengeChatDto dto) {
-      return challengeChatMapper.insertAccountChatroom(dto);
+        return challengeChatMapper.insertAccountChatroom(dto);
     }
-    
-	
-    //챌린지 중복 확인
+
+
+    // 챌린지 중복 확인
     public int selectchallengeAccount(ChallengeChatDto dto) {
         return challengeChatMapper.findChallengeAccount(dto);
     }
-	
-    //챌린지 계정 생성
+
+    // 챌린지 계정 생성
     @Transactional
     public int createAccountChallenge(ChallengeChatDto dto) {
-      try {
-          challengeChatMapper.insertAccountChallengeMemeber(dto);
-          return challengeChatMapper.insertAccountChatroom(dto);
-      } catch (Exception e) {
-          TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
-          return -1;
-      }
+        try {
+            challengeChatMapper.insertAccountChallengeMemeber(dto);
+            return challengeChatMapper.insertAccountChatroom(dto);
+        } catch (Exception e) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return -1;
+        }
     }
-    
-    //채팅 저장
+
+    // 채팅 저장
     public int chatRoomSave(ChallengeChatDto message) {
-      return challengeChatMapper.insertChatMessage(message);
+        return challengeChatMapper.insertChatMessage(message);
     }
-    
-    //채팅 닉네임 아이디 구하기(임시)
+
+    // 채팅 닉네임 아이디 구하기(임시)
     public long chatIdSave(ChallengeChatDto message) {
-      return challengeChatMapper.findUser(message);
+        return challengeChatMapper.findUser(message);
     }
-    
-    //이전의 모든 메세지를 가져오기
+
+    // 이전의 모든 메세지를 가져오기
     public List<ChallengeChatDto> getAllPrevMessage(ChallengeChatDto dto) {
-      return challengeChatMapper.findAllChatByRoomType(dto);
+        return challengeChatMapper.findAllChatByRoomType(dto);
     }
 
-    //챌린지 등수
-    public List<ChallengeChatDto> getChallengeSuccessList(ChallengeChatDto dto) {
-      return challengeChatMapper.findChallengeSuccessList(dto);
+    // 챌린지 등수
+    public List<ChallengeChatDto> getChallengeSuccessList(int chatroomId) {
+        return challengeChatMapper.findChallengeSuccessList(chatroomId);
     }
 
-    //당일 챌린지 성공 횟수
+    // 당일 챌린지 성공 횟수
     public int countChallengeSuccess(ChallengeChatDto dto) {
-      return challengeChatMapper.findChallengeSuccessCount(dto);
+        return challengeChatMapper.findChallengeSuccessCount(dto);
     }
 
-    //챌린지 성공 저장
-    public int saveChallengeSuccess(ChallengeChatDto dto) {     
-      return challengeChatMapper.insertChallengeSuccessCount(dto);
+    // 챌린지 성공 저장
+    public int saveChallengeSuccess(ChallengeChatDto dto) {
+        return challengeChatMapper.insertChallengeSuccessCount(dto);
     }
-    
-    
-    //댓글 저장
+
+
+    // 댓글 저장
     @Transactional
-	public int savechallengeComment(ChallengeCommentDto dto) {
+    public int savechallengeComment(ChallengeCommentDto dto) {
         try {
             challengeChatMapper.insertComment(dto);
             return challengeChatMapper.insertChallengeComment(dto);
@@ -106,33 +104,29 @@ public class ChallengeChatService {
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             return -1;
         }
-	}
-
-    
-  //챌린지 댓글 가지고 오기
-	public List<ChallengeCommentDto> getChallengeCommentList(ChallengeCommentDto dto) {
-		return challengeChatMapper.findCommentList(dto);
-	}
-
-	//챌린지 댓글 수정
-	public int editChallengeComment(ChallengeCommentDto dto) {
-		return challengeChatMapper.updateComment(dto);
-	}
-
-	//챌린지 댓글 삭제
-	public int deleteChallengeComment(ChallengeCommentDto dto) {
-		return challengeChatMapper.deleteComment(dto);
-	}
-
-	//챌린지 유저 nick찾기 
-	public Map findUserNickByUserId(ChallengeCommentDto dto) {
-		return challengeChatMapper.selectUserNick(dto);
-	}
+    }
 
 
+    // 챌린지 댓글 가지고 오기
+    public List<ChallengeCommentDto> getChallengeCommentList(ChallengeCommentDto dto) {
+        return challengeChatMapper.findCommentList(dto);
+    }
+
+    // 챌린지 댓글 수정
+    public int editChallengeComment(ChallengeCommentDto dto) {
+        return challengeChatMapper.updateComment(dto);
+    }
+
+    // 챌린지 댓글 삭제
+    public int deleteChallengeComment(ChallengeCommentDto dto) {
+        return challengeChatMapper.deleteComment(dto);
+    }
+
+    // 챌린지 유저 nick찾기
+    public Map findUserNickByUserId(ChallengeCommentDto dto) {
+        return challengeChatMapper.selectUserNick(dto);
+    }
 
 
 
-
-	
 }
